59219b6233e077a77f09e205da718963b661eb28
KingSeth982 <55211035+KingSeth982@users.noreply.github.com>
2020-11-04 16:28:24 -0600

    Added theHarvester support

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # auto-recon
-Script for running tasks in recon-ng
+
+## Table of contents
+* [General info](#general-info)
+* [Technologies](#technologies)
+* [Setup](#install-and-setup)
+
+## General info
+Script for running through modules in recon-ng with little input from the user and then
+export the the tables from recon-ng's database to an .xlsx
+
+	
+## Technologies:
+Project is created with:
+* [Python](https://www.python.org/downloads/release/python-379/) version: 3.7+
+* [Recon-ng](https://github.com/lanmaster53/recon-ng) version: 5.1.1
+* [theHarvester](https://github.com/laramies/theHarvester) version: Lastest
+	
+## Install and Setup:
+To run this project, first install the pip requirements in auto-recon, recon-ng, and theHarvester.  
+
+```
+$ python3 -m pip install -r auto-recon/requirements.txt
+$ python3 -m pip install -r recon-ng/REQUIREMENTS
+$ python3 -m pip install -r theHarvester/requirements/base.txt
+```
+           
--- auto-recon.py
+++ auto-recon.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3.7
+#!/usr/bin/env python3
 
 import argparse
 import sys
@@ -8,14 +8,19 @@ import os
 import subprocess
 import re
 from lxml import etree
+from configparser import ConfigParser
 
+# Imports the config file
 try:
-	from config import *
+	config_name = 'config.ini'
+	config = ConfigParser()
+	config.read(os.path.join(os.path.dirname(__file__),config_name))
 except:
-	print("Config.py was not found")
-	reconPath = "~/recon-ng/"
+	print("config.ini was not found, exiting...")
+	exit()
 
-sys.path.insert(0,reconPath)
+
+sys.path.insert(0,config.get('paths','recon_path'))
 from recon.core import base
 from recon.core.framework import Colors
 
@@ -43,7 +48,7 @@ def run_recon(domains):
 	reconb = base.Recon()
 	options = [base.Mode.CLI, wspace]
 	reconb.start(*options)
-	reconb.onecmd("TIMEOUT 100")
+	reconb.onecmd("options set TIMEOUT 100")
 	
 	module_list = [
 		'recon/domains-hosts/hackertarget',
@@ -51,53 +56,136 @@ def run_recon(domains):
 		'recon/domains-hosts/binaryedge',
 		'recon/hosts-hosts/ipinfodb',
 		'recon/hosts-ports/shodan_ip',
-		'recon/hosts-ports/binaryedge'
+		'recon/hosts-ports/binaryedge',
+		'recon/contacts-credentials/hibp_breach',
+		'recon/contacts-credentials/hibp_paste'
 		]
-		
-	# This 
-	if args.harvestor_xml_file:
-		parse_harvestor(reconb, args.harvestor_xml_file)
+	
+	# Syncs the API keys
+	sync_keys(reconb)
 		
 	# Adds domains into the domain database in recon-ng
 	for domain in domains:
 		reconb.insert_domains(domain=domain, mute=True)
 		
-	if args.companies:
-		run_linkedin(reconb, args.companies)
+	## Start of theHarvester Selection 
+	
+	# This will run and import theHarvester
+	if args.theHarvester or \
+	config['theHarvester']['always_run'].getboolean():
+		run_theHarvester(reconb, domains[0])
 		
+	# This will parse the harvester output files
+	if args.harvester_xml_file:
+		parse_harvester(reconb, args.harvester_xml_file)
+		
+	# This will import the seperate host and email files from theHarvester 
 	if args.email_file and args.host_file:
 		load_hosts(reconb, args.host_file)
 		load_email(reconb, args.email_file)
+	
+	## End of theHarvester Selection	
+		
+	# Runs the linkedin module if there is a specified company
+	if args.companies:
+		run_linkedin(reconb, args.companies)
+		
+	# Runs mangle if needed
+	if (args.harvester_xml_file or (args.email_file and args.host_file)\
+	or args.companies):
 		if args.pattern:
 			for domain in domains:
 				run_mangle(reconb, domain, args.pattern)
-	
+		
 	# Runs each module inside the module list
 	for module in module_list:
 		#print("Module: %s, Domain: %s" % (module, domain))
 		run_module(reconb, module, domain)
-			
+					
 	#Exports the DB to an excel file	
 	export_Excel(reconb, args.export)
 
-#Adds key for the config file if there is one present
-def add_keys():
-	reconBase = base.Recon()
-	options = [base.Mode.CLI]
-	reconBase.start(*options)
-	for api_keys in keys:
-		if not api_keys[1] == '':
-			reconBase.onecmd("keys add %s %s" % (api_keys[0], api_keys[1]))
+# Updates the api keys stored in recon-ng
+def sync_keys(reconBase=''):
+	options = config['options']
+	imput = False
+	if reconBase == '':
+		imput = True
+		reconBase = base.Recon()
+		recon_options = [base.Mode.CLI]
+		reconBase.start(*recon_options)
+		
+	print(not imput and options.getboolean('key_auto_update'))
 	
-			
-def parse_harvestor(reconb, filename):
+	if (not imput and options.getboolean('key_auto_update'))\
+	or imput:
+		recon = reconBase
+		recon_keys = recon._query_keys('select * from keys')
+		config_keys = config['apiKeys']
+		config_changed = False
+		
+		# Compares the recon-ng keys to the config keys
+		for recon_key in recon_keys:
+			for config_key in config_keys.items():
+				if recon_key[0] == config_key[0]:
+					
+					# This section runs if one of the keys is blank, but
+					# not both of them are blank
+					if (recon_key[1] == '' or config_key[1] == '') \
+					and not(recon_key[1] == '' and config_key[1] == ''):
+						if recon_key[1] == '':
+							recon.add_key(config_key[0], config_key[1])
+							config_changed = True
+						elif config_keys[recon_key[0]] == '':
+							config_keys[config_key[0]] = recon_key[1]
+							config_changed = True
+							
+					# This section runs if both files has a key, the keys
+					# are not the same and the key_override_from_master
+					# is set to true 
+					elif (recon_key[1] != config_keys[recon_key[0]]) and \
+					options.getboolean('key_override_from_master')\
+					and not(recon_key[1] == '' and config_keys[recon_key[0]] == ''):
+						if options.getboolean('key_master_config_file'):
+							recon.add_key(config_key[0], config_keys[1])
+							config_changed = True
+						else:
+							config_keys[config_key[0]] = recon_key[1]
+							config_changed = True
+							
+		# Writed the changes to the ini file if there is an update	
+		if config_changed:
+			with open(dir_file(config_name), 'w') as configfile:
+				config.write(configfile)
+	# Clears variables from memeory				
+	api_keys = None
+	options = None
+	config_changed = None
+	
+def run_theHarvester(reconbase, domain):
+	theHarvester = config['theHarvester']
+	previous_path = os.getcwd() + '/'
+	filename = theHarvester['temp_filename']
+	
+	os.chdir(theHarvester['path'])
+	subprocess.run("\'{}theHarvester.py\' -b {} -d {} \
+	-f {}".format(theHarvester['path'], theHarvester['source'],\
+	domain, "'" + previous_path + filename + ".'"), shell=True)
+	os.chdir(previous_path)
+	parse_harvester(reconbase, filename + '.xml')
+	
+	# Clears variables from memeory
+	previous_path = None
+	filename = None
+		
+def parse_harvester(reconBase, filename):
 	tree = etree.parse(filename)
 	for element in tree.iter("email"):
-		reconb.insert_contacts(email=element.text)
+		reconBase.insert_contacts(email=element.text)
 	for element in tree.iter("hostname"):
-		reconb.insert_hosts(host=element.text)
+		reconBase.insert_hosts(host=element.text)
 				
-def load_email(reconb, filename):
+def load_email(reconBase, filename):
 	recon = module_load(reconBase, "import/list")
 	recon._do_options_set("FILENAME " + filename)
 	recon._do_options_set("TABLE contacts")
@@ -105,7 +193,7 @@ def load_email(reconb, filename):
 	recon.do_run(None)
 	
 	
-def load_hosts(reconb, filename):
+def load_hosts(reconBase, filename):
 	recon = module_load(reconBase, "import/csv")
 	recon._do_options_set("FILENAME " + filename)
 	recon._do_options_set("HAS-HEADER false")
@@ -128,9 +216,8 @@ def run_mangle(reconBase, domain, pattern):
 	recon = module_load(reconBase, 'recon/contacts-contacts/mangle')
 	recon._do_options_set("SOURCE " + domain)
 	recon._do_options_set("PATTERN " + pattern)
-	recon._do_options_set("MAX-LENGTH 40")
-	recon.do_run(None)
-	
+	recon._do_options_set("MAX-LENGTH 60")
+	recon.do_run(None)	
 
 def export_Excel(reconBase, filename):
 	recon = module_load(reconBase, 'reporting/xlsx')
@@ -148,7 +235,9 @@ def module_load(reconBase, module):
 		return recon
 	else:
 		print("Automatic installation of module %s failed. Please install the module manualy")
-	
+
+def dir_file(filename):
+	return os.path.join(os.path.dirname(__file__),filename)
 
 # Installs the required pip module for the recon-ng modules to work
 def pip_package_install(pip_packages):
@@ -159,19 +248,27 @@ parser = argparse.ArgumentParser(
 	description='Automation script for recon-ng')
 parser.add_argument("-w", "--workspace", dest="workspace", metavar="<name>", help="name of workspace inside recon-ng")
 parser.add_argument("-d", dest="filename", metavar='<filename>', type=argparse.FileType('r'), help="input file of domains (one per line)", default=None)
-parser.add_argument("-e", dest="email_file", metavar='<filename>', type=argparse.FileType('r'), help="input file of emails from harvester", default=None)
-parser.add_argument("-i", dest="host_file", metavar='<filename>', type=argparse.FileType('r'), help="input file of hosts from harvester", default=None)
-parser.add_argument("-p", dest="pattern", help="pattern to whatever matches the emails seen from harvester", default=None)
-parser.add_argument("-H", "--harvest", metavar='<filename>', dest='harvestor_xml_file', help="xml file from harvestor")
-parser.add_argument("-l", '--linkedin', action='append', dest='companies', help="name of the compaony to be searched on linkedin")
+parser.add_argument("-p", dest="pattern", help='Pattern options: <fi>,<fn>,<mi>,<mn>,<li>,<ln>', default=None)
+parser.add_argument("-l", '--linkedin', action='append', dest='companies', help="name of the company to be searched on linkedin")
 parser.add_argument("-E", "--export", metavar='<filename>', dest='export', help="name of the export file, default is output.xlsx", default="output.xlsx")
-parser.add_argument("--pip", dest='pip', action='store_true', help="installs the required pip modules for the script to work")
 parser.add_argument("domains", help="one or more domains", nargs="*", default=None)
-parser.add_argument("-k", "--keys", dest="keys", action='store_true', help="adds api keys from config file if present")
+
+# theHarvester argument group
+theHarvester_group = parser.add_argument_group('theHarvester', 'Commands to interact with theHarvester, if installed.')
+theHarvester_group.add_argument("--theHarvester", help="Run theHarvester with the domains again google, yahoo, duckduckgo, and bing", action='store_true')
+theHarvester_group.add_argument("-H", "--harvest", metavar='<filename>', dest='harvester_xml_file', help="xml file from harvester")
+theHarvester_group.add_argument("-e", dest="email_file", metavar='<filename>', type=argparse.FileType('r'), help="input file of emails from harvester", default=None)
+theHarvester_group.add_argument("-i", dest="host_file", metavar='<filename>', type=argparse.FileType('r'), help="input file of hosts from harvester", default=None)
+
+# Setup argument group
+setup_group = parser.add_argument_group('Optional Setup', 'Run these to automate some setup.')
+setup_group.add_argument("-k", "--keys", dest="keys", action='store_true', help="adds api keys from config file if present")
+setup_group.add_argument("--pip", dest='pip', action='store_true', help="installs the required pip modules for the script to work")
 
 args = parser.parse_args()
 
 domainList = []
+
 
 if args.export:
 	if not args.export.endswith('.xlsx'):
@@ -179,7 +276,7 @@ if args.export:
 		
 		
 if args.keys:
- add_keys()
+	sync_keys()
 
 if args.domains:
 	domainList+=args.domains
