ed3205afa3a9327b87735a2d094977c5d96e01b6
KingSeth982 <55211035+KingSeth982@users.noreply.github.com>
2020-11-09 14:32:30 -0600

    Fixed Bugs and Config Layout

--- auto-recon.py
+++ auto-recon.py
@@ -20,7 +20,7 @@ except:
 	exit()
 
 
-sys.path.insert(0,config.get('paths','recon_path'))
+sys.path.insert(0,config.get('recon-ng','path'))
 from recon.core import base
 from recon.core.framework import Colors
 
@@ -30,6 +30,7 @@ package_list = [
 	'lxml',
 	'shodan'
 	]
+
 	
 def run_module(reconBase, module, domain):
 	recon = module_load(reconBase, module)
@@ -51,6 +52,7 @@ def run_recon(domains):
 	reconb.onecmd("options set TIMEOUT 100")
 	
 	module_list = [
+		'recon/companies-multi/whois_miner',
 		'recon/domains-hosts/hackertarget',
 		'recon/domains-hosts/brute_hosts',
 		'recon/domains-hosts/binaryedge',
@@ -72,7 +74,7 @@ def run_recon(domains):
 	
 	# This will run and import theHarvester
 	if args.theHarvester or \
-	config['theHarvester']['always_run'].getboolean():
+	config.getboolean('theHarvester', 'always_run'):
 		run_theHarvester(reconb, domains[0])
 		
 	# This will parse the harvester output files
@@ -92,7 +94,7 @@ def run_recon(domains):
 		
 	# Runs mangle if needed
 	if (args.harvester_xml_file or (args.email_file and args.host_file)\
-	or args.companies):
+	or args.companies or args.theHarvester):
 		if args.pattern:
 			for domain in domains:
 				run_mangle(reconb, domain, args.pattern)
@@ -105,18 +107,17 @@ def run_recon(domains):
 	#Exports the DB to an excel file	
 	export_Excel(reconb, args.export)
 
+
 # Updates the api keys stored in recon-ng
 def sync_keys(reconBase=''):
-	options = config['options']
+	options = config['recon-ng']
 	imput = False
 	if reconBase == '':
 		imput = True
 		reconBase = base.Recon()
 		recon_options = [base.Mode.CLI]
 		reconBase.start(*recon_options)
-		
-	print(not imput and options.getboolean('key_auto_update'))
-	
+			
 	if (not imput and options.getboolean('key_auto_update'))\
 	or imput:
 		recon = reconBase
@@ -124,43 +125,51 @@ def sync_keys(reconBase=''):
 		config_keys = config['apiKeys']
 		config_changed = False
 		
-		# Compares the recon-ng keys to the config keys
-		for recon_key in recon_keys:
+		# Handles if no modules has been loaded beofre running
+		if recon_keys == []:
 			for config_key in config_keys.items():
-				if recon_key[0] == config_key[0]:
+				recon.add_key(config_key[0], config_key[1])
+						
+		# This will deal with most other scenarios
+		else:
+			# Compares the recon-ng keys to the config keys
+			for recon_key in recon_keys:
+				for config_key in config_keys.items():
+					if recon_key[0] == config_key[0]:
 					
-					# This section runs if one of the keys is blank, but
-					# not both of them are blank
-					if (recon_key[1] == '' or config_key[1] == '') \
-					and not(recon_key[1] == '' and config_key[1] == ''):
-						if recon_key[1] == '':
-							recon.add_key(config_key[0], config_key[1])
-							config_changed = True
-						elif config_keys[recon_key[0]] == '':
-							config_keys[config_key[0]] = recon_key[1]
-							config_changed = True
-							
-					# This section runs if both files has a key, the keys
-					# are not the same and the key_override_from_master
-					# is set to true 
-					elif (recon_key[1] != config_keys[recon_key[0]]) and \
-					options.getboolean('key_override_from_master')\
-					and not(recon_key[1] == '' and config_keys[recon_key[0]] == ''):
-						if options.getboolean('key_master_config_file'):
-							recon.add_key(config_key[0], config_keys[1])
-							config_changed = True
-						else:
-							config_keys[config_key[0]] = recon_key[1]
-							config_changed = True
-							
-		# Writed the changes to the ini file if there is an update	
-		if config_changed:
-			with open(dir_file(config_name), 'w') as configfile:
-				config.write(configfile)
+						# This section runs if one of the keys is blank, but
+						# not both of them are blank
+						if (recon_key[1] == '' or config_key[1] == '') \
+						and not(recon_key[1] == '' and config_key[1] == ''):
+							if recon_key[1] == '':
+								recon.add_key(config_key[0], config_key[1])
+								config_changed = True
+							elif config_keys[recon_key[0]] == '':
+								config_keys[config_key[0]] = recon_key[1]
+								config_changed = True
+								
+						# This section runs if both files has a key, the keys
+						# are not the same and the key_override_from_master
+						# is set to true 
+						elif (recon_key[1] != config_keys[recon_key[0]]) and \
+						options.getboolean('key_override_from_master')\
+						and not(recon_key[1] == '' and config_keys[recon_key[0]] == ''):
+							if options.getboolean('key_master_config_file'):
+								recon.add_key(config_key[0], config_keys[1])
+								config_changed = True
+							else:
+								config_keys[config_key[0]] = recon_key[1]
+								config_changed = True
+								
+			# Writed the changes to the ini file if there is an update	
+			if config_changed:
+				with open(dir_file(config_name), 'w') as configfile:
+					config.write(configfile)
 	# Clears variables from memeory				
 	api_keys = None
 	options = None
 	config_changed = None
+
 	
 def run_theHarvester(reconbase, domain):
 	theHarvester = config['theHarvester']
@@ -177,6 +186,7 @@ def run_theHarvester(reconbase, domain):
 	# Clears variables from memeory
 	previous_path = None
 	filename = None
+
 		
 def parse_harvester(reconBase, filename):
 	tree = etree.parse(filename)
@@ -184,6 +194,10 @@ def parse_harvester(reconBase, filename):
 		reconBase.insert_contacts(email=element.text)
 	for element in tree.iter("hostname"):
 		reconBase.insert_hosts(host=element.text)
+	# Clears variables from memeory
+	tree = None
+	element = None
+
 				
 def load_email(reconBase, filename):
 	recon = module_load(reconBase, "import/list")
@@ -219,6 +233,7 @@ def run_mangle(reconBase, domain, pattern):
 	recon._do_options_set("MAX-LENGTH 60")
 	recon.do_run(None)	
 
+
 def export_Excel(reconBase, filename):
 	recon = module_load(reconBase, 'reporting/xlsx')
 	recon._do_options_set("filename " + filename)
@@ -236,20 +251,23 @@ def module_load(reconBase, module):
 	else:
 		print("Automatic installation of module %s failed. Please install the module manualy")
 
+
 def dir_file(filename):
 	return os.path.join(os.path.dirname(__file__),filename)
+
 
 # Installs the required pip module for the recon-ng modules to work
 def pip_package_install(pip_packages):
 	for package in pip_packages:
 		subprocess.check_call([sys.executable, '-m', 'pip', 'install', package])
 
+
 parser = argparse.ArgumentParser(
 	description='Automation script for recon-ng')
 parser.add_argument("-w", "--workspace", dest="workspace", metavar="<name>", help="name of workspace inside recon-ng")
 parser.add_argument("-d", dest="filename", metavar='<filename>', type=argparse.FileType('r'), help="input file of domains (one per line)", default=None)
 parser.add_argument("-p", dest="pattern", help='Pattern options: <fi>,<fn>,<mi>,<mn>,<li>,<ln>', default=None)
-parser.add_argument("-l", '--linkedin', action='append', dest='companies', help="name of the company to be searched on linkedin")
+parser.add_argument("-c", '--company', action='append', dest='companies', help="name of the company")
 parser.add_argument("-E", "--export", metavar='<filename>', dest='export', help="name of the export file, default is output.xlsx", default="output.xlsx")
 parser.add_argument("domains", help="one or more domains", nargs="*", default=None)
 
@@ -269,12 +287,10 @@ args = parser.parse_args()
 
 domainList = []
 
-
 if args.export:
 	if not args.export.endswith('.xlsx'):
 		args.export = args.export + '.xlsx'
-		
-		
+			
 if args.keys:
 	sync_keys()
 
